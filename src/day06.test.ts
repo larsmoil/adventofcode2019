2b1cc8d5e66929c43ac01f5b11bcef32bbc4c38c
Lars-Olof Moilanen <lo@jpro.no>
2019-12-16 22:08:58 +0100

    Day 6 part 2

--- src/day06.test.ts
+++ src/day06.test.ts
@@ -1,11 +1,41 @@
 import test from 'ava';
-import { program } from "./day06";
+import { programPt1, programPt2 } from "./day06";
 import { readFileSync } from "fs";
 import { resolve } from "path";
 
-test('day 6 - example', (t) => {
+test('day 6 - part 2', (t) => {
+  const lines: string[] = readFileSync(resolve(__dirname, "..", "puzzle-inputs", "day06.txt")).toString().split(/\n/).filter(Boolean);
   t.deepEqual(
-    program([
+    programPt2(lines),
+    409
+  );
+});
+
+
+test('day 6 - part 2 example', (t) => {
+  t.deepEqual(
+    programPt2([
+      'COM)B',
+      'B)C',
+      'C)D',
+      'D)E',
+      'E)F',
+      'B)G',
+      'G)H',
+      'D)I',
+      'E)J',
+      'J)K',
+      'K)L',
+      'K)YOU',
+      'I)SAN',
+    ]),
+    4
+  );
+});
+
+test('day 6 - part 1 example', (t) => {
+  t.deepEqual(
+    programPt1([
       'COM)B',
       'B)C',
       'C)D',
@@ -19,13 +49,13 @@ test('day 6 - example', (t) => {
       'K)L',
     ]),
     42
-  )
+  );
 });
 
 test('day 6 - part 1', (t) => {
   const lines: string[] = readFileSync(resolve(__dirname, "..", "puzzle-inputs", "day06.txt")).toString().split(/\n/).filter(Boolean);
   t.deepEqual(
-    program(lines),
+    programPt1(lines),
     268504
   );
 });
--- src/day06.ts
+++ src/day06.ts
@@ -1,15 +1,17 @@
 import { readFileSync } from "fs";
 
-export const program: (lines: string[]) => number = (lines) => {
-  const connections: { [k: string]: string[] } = lines.reduce((o, c) => {
+export const buildGraph: (lines: string[]) => { [k: string]: string[] } = (lines) => {
+  return lines.reduce((o, c) => {
     const [from, to] = c.split(')');
     return {
       ...o,
       [to]: (o[to] || []).concat([from]),
-
     };
   }, {} as { [k: string]: string[]});
+};
 
+export const programPt1: (lines: string[]) => number = (lines) => {
+  const connections = buildGraph(lines);
   const numConnections = (connection: string): number => {
     if (connection === 'COM') {
       return 0;
@@ -24,11 +26,36 @@ export const program: (lines: string[]) => number = (lines) => {
   }, 0);
 };
 
-export const programPt1: (lines: string[]) => number = (lines) => program(lines);
-export const programPt2: (lines: string[]) => number = (lines) => program(lines);
+export const programPt2: (lines: string[]) => number = (lines) => {
+  const connections = buildGraph(lines);
+  const shortestPath = (from: string, to: string, visited: string[]): number => {
+    const incomingConnections: string[] = Object.keys(connections)
+      .filter((c) => {
+        return connections[c].includes(from);
+      })
+      .reduce((a, e) => a.concat([e]), [] as string[]);
+    const directConnections = (connections[from] || [])
+      .concat(incomingConnections)
+      .filter((c) => c !== from)
+      .filter((c) => !visited.includes(c));
+
+    if (directConnections.includes(to)) {
+      return 0;
+    }
+    if (!directConnections) {
+      return Number.MAX_SAFE_INTEGER;
+    }
+
+    return 1 + Math.min(...directConnections.map((c) => shortestPath(c, to, visited.concat(c))));
+  };
+
+  const santaConnections = connections['SAN'];
+  return Math.min(...santaConnections.map((c) => shortestPath('YOU', c, [])));
+
+};
 
 if (process.mainModule && process.mainModule.filename === __filename) {
   const lines = readFileSync(0).toString().split(/\n/).filter(Boolean);
 
-  console.log(`${ programPt1(lines) } (pt.1) / [${ programPt2(lines) }] (pt.2)`);
+  console.log(`${ programPt1(lines) } (pt.1) / ${ programPt2(lines) } (pt.2)`);
 }
